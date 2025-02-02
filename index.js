f25ede8d3f8d685f1b17a82dd3e977196fb22d07
Autumn Lydon <autumnlydon@gmail.com>
2025-02-02 12:00:00 -0500

    update

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-// Solution
+// My Solution:
 
-const solution = (numbers) => {
-  const fakeNums = [...numbers];
-  const newArr = [];
-  for (let i = 0; i < numbers.length; i++) {
-    if (newArr.length % 2 === 0) {
-      newArr.push(fakeNums.shift());
-    } else if (newArr.length % 2 !== 0) {
-      newArr.push(fakeNums.pop());
-    }
-  }
-  return newArr;
-};
+// const solution = (numbers) => {
+//   const fakeNums = [...numbers];
+//   const newArr = [];
+//   for (let i = 0; i < numbers.length; i++) {
+//     if (newArr.length % 2 === 0) {
+//       newArr.push(fakeNums.shift());
+//     } else if (newArr.length % 2 !== 0) {
+//       newArr.push(fakeNums.pop());
+//     }
+//   }
+//   return newArr;
+// };
+
+// Carmen's Solution:

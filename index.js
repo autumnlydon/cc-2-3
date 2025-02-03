1d8b1e294f51040820bb1f70036f86b1f0b63449
Autumn Lydon <autumnlydon@gmail.com>
2025-02-03 09:50:08 -0500

    solution

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Solution
+
+const solution = (numbers) => {
+  const fakeNums = [...numbers];
+  const newArr = [];
+  for (let i = 0; i < numbers.length; i++) {
+    if (newArr.length % 2 === 0) {
+      newArr.push(fakeNums.shift());
+    } else if (newArr.length % 2 !== 0) {
+      newArr.push(fakeNums.pop());
+    }
+  }
+  return newArr;
+};
